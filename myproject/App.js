02d8ddb663ecaa26a15fd7913d1f07130c1ee33e
stephen <nwabuezesc@gmail.com>
2022-09-27 15:13:47 +0100

    react native views, text and styles

--- myproject/App.js
+++ myproject/App.js
@@ -2,23 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, StyleSheet, Text, ScrollView, View } from 'react-native';
 
-// in react native when we pass a function to onchange text, it takes in the value inside the text
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: 'shaun', key: '1' },
-    {name: 'yoshi', key: '2'},
-    {name: 'peach', key: '3'},
-    {name: 'mario', key: '4'},
-    {name: 'luigi', key: '5'},
-    {name: 'toad', key: '6'},
-    {name: 'bowser', key: '7'},
-  ]);
-
   return (
-    // A map function cycle through the array and returns each function in the array
     <View style={styles.container}>
-      <Text>we are working on a react native project</Text>
+      <Text style={styles.boldText}>we are very original</Text>
     </View>
   );
 }
@@ -30,10 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24
+  boldText: {
+    fontWeight: 'bold'
   }
 });
---
+++ myproject/readMe.md
@@ -0,0 +1,6 @@
+# Some Explanation on react native
+ in react native when we pass a function to onchange text, it takes in the value inside the text
+
+ A map function cycle through the array and returns each function in the array and a style prop is a way to style a component
+
+ A view style can not pass down to a text
