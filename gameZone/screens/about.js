316e345b10383ee8a89ee265426810b87b9d2a5c
stephen <nwabuezesc@gmail.com>
2022-10-21 11:17:26 +0100

    adding some screens to the mobile app

---
+++ gameZone/screens/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function about() {
+  return (
+    <View style={styles.container}>
+      <Text>about</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding
+  },
+});
