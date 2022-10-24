fca9964f11645457de69990ca281647b00b92067
stephen <nwabuezesc@gmail.com>
2022-10-24 14:58:10 +0100

    debugging the custom font section

--- gameZone/App.js
+++ gameZone/App.js
@@ -1,33 +1,8 @@
 import React, { useCallback } from 'react';
 import Home from './screens/home'
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
-
-// const getFonts = () => useFonts.loadAsync({
-//   'nunito-regular': require('./assets/fonts/static/Nunito-Regular.ttf'),
-//   'nunito-bold': require('./assets/fonts/static/Nunito-Bold.ttf')
-// });
+// import { useFonts } from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'nunito-regular': require('./assets/fonts/static/Nunito-Regular.ttf'),
-    'nunito-bold': require('./assets/fonts/static/Nunito-Bold.ttf')
-  })
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-  
-  if(!fontsLoaded){
-    return null
-  } else {
-    <Home onLayout={onLayoutRootView}/>
-  }
-
+  <Home/>
 }
--- gameZone/screens/home.js
+++ gameZone/screens/home.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
-export default function Home(onLayout) {
+export default function Home() {
   return (
-    <View style={styles.container} onLayout={onLayout}>
-      <Text>Home</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 30
-  },
-  title: {
-    fontFamily: 'nunito-bold',
-    fontSize: 30
-  }
-});
 
